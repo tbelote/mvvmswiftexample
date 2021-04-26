cd630bd0448f531e26c81c76fe76e755c6a07cfa
Tom Belote <tbelote@me.com>
2021-04-26 10:58:53 -0400

    import

---
+++ MVVMSwiftExample/GameScoreboardEditorViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  GameScoreboardEditorViewModel.swift
+//  MVVMSwiftExample
+//
+//  Created by Dino Bartosak on 28/01/2017.
+//  Copyright © 2017 Toptal. All rights reserved.
+//
+
+import Foundation
+
+protocol GameScoreboardEditorViewModel {
+    var homeTeam: String { get }
+    var awayTeam: String { get }
+    var time: Dynamic<String> { get }
+    var score: Dynamic<String> { get }
+    var isFinished: Dynamic<Bool> { get }
+    
+    var isPaused: Dynamic<Bool> { get }
+    func togglePause()
+    
+    var homePlayers: [PlayerScoreboardMoveEditorViewModel] { get }
+    var awayPlayers: [PlayerScoreboardMoveEditorViewModel] { get }
+}
---
+++ MVVMSwiftExample/PlayerScoreboardMoveEditorViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerScoreboardMoveEditorViewModel.swift
+//  MVVMSwiftExample
+//
+//  Created by Dino Bartosak on 28/01/2017.
+//  Copyright © 2017 Toptal. All rights reserved.
+//
+
+import Foundation
+
+protocol PlayerScoreboardMoveEditorViewModel {
+    var playerName: String { get }
+    
+    var onePointMoveCount: Dynamic<String> { get }
+    var twoPointMoveCount: Dynamic<String> { get }
+    var assistMoveCount: Dynamic<String> { get }
+    var reboundMoveCount: Dynamic<String> { get }
+    var foulMoveCount: Dynamic<String> { get }
+    
+    func onePointMove()
+    func twoPointsMove()
+    func assistMove()
+    func reboundMove()
+    func foulMove()
+}
